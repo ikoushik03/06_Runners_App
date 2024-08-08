02c7a870a3f1a37b53fa667a2b59de4fbc8cfa88
hp <koushiksharma9939@gmail.com>
2024-08-08 21:48:26 +0530

    first commit

---
+++ src/main/java/com/koushik/runner/MyCmdRunner.java
@@ -0,0 +1,16 @@
+package com.koushik.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class MyCmdRunner implements CommandLineRunner{
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		System.out.println("AppCmd runner :: run() executed");
+	}
+
+}
